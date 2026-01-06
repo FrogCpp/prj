9dbb5430c822f98c6211d0b7985bfe1d75250cfd
a <161460644+FrogCpp@users.noreply.github.com>
2026-01-06 20:59:45 +0300

    loadScene

---
+++ Assets/Scenes/Game/Menu/LoaderScripts/MainLoaderScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainLoaderScript : MonoBehaviour
+{
+    [SerializeField] private Slider progressBar;
+    [SerializeField] private TMP_Text progressText;
+
+    void Start()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        string sceneName = SceneLoaderData.SceneToLoad;
+
+        if (string.IsNullOrEmpty(sceneName))
+        {
+            sceneName = "MenuScene";
+            Debug.LogWarning("Scene to load was not specified. Loading MainMenu.");
+        }
+        AsyncOperation operation;
+        try
+        {
+            operation = SceneManager.LoadSceneAsync(sceneName);
+            operation.allowSceneActivation = false;
+        }
+        catch
+        {
+            operation = SceneManager.LoadSceneAsync("MenuScene");
+            operation.allowSceneActivation = false;
+        }
+
+        while (!operation.isDone)
+        {
+            float progress = operation.progress;
+
+            if (progressBar != null)
+                progressBar.value = progress;
+
+            if (progressText != null)
+                progressText.text = Mathf.Round(progress * 100) + "%";
+
+            if (operation.progress >= 0.9f)
+            {
+                yield return new WaitForSeconds(0.5f);
+                operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
+
+
+public static class SceneLoaderData
+{
+    public static string SceneToLoad { get; set; }
+}
---
+++ Assets/Scenes/Game/Menu/MenuScript/InfoBoardControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InfoBoardControl : MonoBehaviour
+{
+    private TMP_Text text;
+
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();
+
+        text.text = "Вы — подводный психотерапевт, готовый погрузиться в пучины сознания морских обитателей.\r\nКаждая новая рыбка — новая история, спрятанная среди кораллов страхов и водорослей воспоминаний.";
+    }
+
+    public void Press(GameObject btn)
+    {
+        switch (btn.name)
+        {
+            case "LevelSprite (0)":
+                text.text = "ВСТАВИТЬ ОПИСАНИЕ! 1";
+                break;
+            case "LevelSprite (1)":
+                text.text = "ВСТАВИТЬ ОПИСАНИЕ! 2";
+                break;
+            case "LevelSprite (2)":
+                text.text = "ВСТАВИТЬ ОПИСАНИЕ! 3";
+                break;
+            case "LevelSprite (3)":
+                text.text = "ВСТАВИТЬ ОПИСАНИЕ! 4";
+                break;
+            case "LevelSprite (4)":
+                text.text = "ВСТАВИТЬ ОПИСАНИЕ! 5";
+                break;
+        }
+    }
+
+
+    public void LoadScene(GameObject btn)
+    {
+        switch (btn.name)
+        {
+            case "1":
+                SceneLoaderData.SceneToLoad = "Level1";
+                break;
+            case "2":
+                SceneLoaderData.SceneToLoad = "Level2";
+                break;
+            case "3":
+                SceneLoaderData.SceneToLoad = "Level3";
+                break;
+            case "4":
+                SceneLoaderData.SceneToLoad = "Level4";
+                break;
+            case "5":
+                SceneLoaderData.SceneToLoad = "Level5";
+                break;
+        }
+
+        SceneManager.LoadScene("Load");
+    }
+}
